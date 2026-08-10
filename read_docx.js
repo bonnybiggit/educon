2a3ac617a1c6cc9dc088c7f060aeb9deb0a0775a
bonnybiggit <togun39s@gmail.com>
2026-08-10 14:51:07 +0100

    Latest update

--- read_docx.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-const path = require('path');
-const AdmZip = require('adm-zip'); // wait, I don't know if adm-zip is installed. Let's use standard library if possible, or install adm-zip
-
-// Actually it's easier to just install adm-zip or mammoth
